1da2b26f42590d4de53932ae84a1ec1fe93773dc
Hemavathianandan <36996181+Hemavathibabes@users.noreply.github.com>
2020-05-21 16:07:14 +0530

    Add files via upload

---
+++ Generic Collections/Generic Collections/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Collections
+{
+
+    internal class student
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List:");
+            List<int> intlist = new List<int>();
+            intlist.Add(12);
+            intlist.Add(34);
+            intlist.Add(56);
+            Console.WriteLine("Integer list");
+            foreach (int i in intlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<string> strlist = new List<string>()
+            { "It" , "is" ,"a" ,"string" , "list" };
+            Console.WriteLine("String List");
+            foreach (String st in strlist)
+            {
+                Console.WriteLine(st);
+            }
+
+            Console.WriteLine("object string");
+            List<student> obj = new List<student>()
+            {
+                new student() { Id = 101, name = "Karthik" },
+                new student() { Id = 102, name = "jessi" }
+
+            };
+            foreach (var item in obj)
+            {
+                Console.WriteLine("{0},{1}", item.Id, item.name);
+            }
+
+            Console.WriteLine();
+
+
+            //Dictionary
+            Console.WriteLine("Dictionary");
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1, "first");
+            dic.Add(2, "second");
+            dic.Add(3, "third");
+
+
+            foreach (var d in dic)
+            {
+                Console.WriteLine("{0},{1}", d.Key, d.Value);
+            }
+
+            Console.WriteLine("the first key value pair is {0},{1}", dic.ElementAt(0).Key, dic.ElementAt(0).Value);
+            Console.WriteLine();
+            IDictionary<int, student> odic = new Dictionary<int, student>()
+            {
+                { 1, new student() { Id = 301, name = "Varun" } },
+                { 2,new student() { Id = 302, name = "Nithya" } }
+            };
+
+            foreach (var od in odic)
+            {
+                Console.WriteLine("{0},{1},{2}", od.Key, od.Value.Id, od.Value.name);
+            }
+            Console.WriteLine();
+
+            //sorted list
+            Console.WriteLine("Sorted List:");
+            SortedList<int, string> sl = new SortedList<int, string>();
+            sl.Add(1, "India");
+            sl.Add(2, "America");
+            sl.Add(3, "china");
+
+            foreach (var i in sl)
+            {
+                Console.WriteLine("{0},{1}", i.Key, i.Value);
+            }
+            Console.WriteLine("the value for 1st key is {0}", sl[1]);
+            Console.WriteLine();
+
+
+            //stack
+
+
+            Console.WriteLine("Stack:");
+            Stack<string> s = new Stack<string>();
+            s.Push("this is");
+            s.Push("generic");
+            s.Push("stack");
+            s.Pop();
+           foreach(String str in s)
+            {
+                Console.WriteLine(str + " ");
+            }
+            Console.WriteLine();
+
+            //Queue
+            Console.WriteLine("Queue:");
+            Queue<float> fl = new Queue<float>();
+            fl.Enqueue(0.3f);
+            fl.Enqueue(.5f);
+            fl.Enqueue(.2f);
+            fl.Dequeue();
+            foreach (float f in fl)
+            {
+                Console.WriteLine(f + " ");
+            }
+            
+        }
+
+    }
+}
+
+   
---
+++ Non-Generic Collections/Non-Generic Collections/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList
+
+
+            ArrayList al = new ArrayList();
+            int a = 101;
+            al.Add(a);
+            al.Add("This is a arraylist");
+            al.Add(2.3f);
+            al.Insert(3, "This is the forth index");
+          
+            ArrayList al2 = new ArrayList();
+            al2.Add(102);
+            al.InsertRange(1, al2);
+            Console.WriteLine("ArrayList:");
+                Console.WriteLine();
+            foreach(var ele in al)
+            {
+                Console.WriteLine(ele + " ");
+            }
+            Console.WriteLine("The count of the arraylist is {0}", al.Count);
+            Console.WriteLine("The capacity of the arraylist is {0}", al.Capacity);
+            Console.WriteLine();
+            // Hash table
+
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "one");
+            ht.Add(2, "two");
+            ht.Add(0.3f, 2);
+            ht.Add(4, null);
+            ht.Add("Hema", "vathi");
+            ht.Remove(2);
+           // ht.Clear();
+            Console.WriteLine("Hashtable:");
+            Console.WriteLine();
+            foreach (DictionaryEntry ele in ht)   //ht.keys means only print a key values for values the viceversa is applied.
+            {
+                Console.WriteLine("{0},{1}", ele.Key, ele.Value);
+                
+            }
+            String firstelement =(string)ht[1];
+            String str = (string)ht["Hema"];
+            Console.WriteLine("The first element in the hashtable is {0}",firstelement);
+            Console.WriteLine("The last element in the hashtable is {0}", str);
+            Console.WriteLine("The hash table contains a 0.3  is {0}", ht.Contains(0.3f));
+            Console.WriteLine();
+
+            //SortedList
+            Console.WriteLine("Sorted List:");
+            SortedList sl = new SortedList();
+            sl.Add(2, "two");
+            sl.Add(1, "one");
+            sl.Add(0, "zero");
+           sl.RemoveAt(0);
+            foreach(DictionaryEntry i in sl)
+            {
+                Console.WriteLine("{0},{1}",i.Key,i.Value);
+            }
+            Console.WriteLine();
+
+            //stack
+
+            Console.WriteLine("Stack:");
+            Stack s = new Stack();
+            s.Push("hello");
+            s.Push(102);
+            s.Push(102);
+            s.Push(.3f);
+            foreach(var item in s)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("The first element will be deleted is {0} ",s.Peek());
+            Console.WriteLine();
+
+            //Queue
+
+            Console.WriteLine("Queue:");
+            Queue q = new Queue();
+
+            q.Enqueue(34);
+            q.Enqueue("thirtyfour");
+            q.Enqueue(3.4);
+            q.Dequeue();
+            foreach(var item in q)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("The first element of the queue is {0}", q.Peek());
+        }
+    }
+}
